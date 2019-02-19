8b15bb499c34e5d3c0d1c096388c203786e82953
Marc Scott <marc@raspberrypi.org>
2019-02-19 13:38:37 +0000

    removed directories

--- en/images/q3/trim_and_tile.sh
+++
@@ -1,7 +0,0 @@
-for file in 3*.png;
-do
-  text=$(echo "$file" | head -c 2 | tail -c 1) #get 2nd character from file name
-  convert $file -trim +repage $file; #trim the image
-  convert $file -gravity South -splice 0x36 -font FreeSans -pointsize 36 -fill red -annotate +0+-5 "$text" label$file; #annotate with text
-done
-montage label*.png -tile 2x2 -geometry +5+1  montage-3.png #montage
